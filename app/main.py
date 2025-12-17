eff90cbd03872ba386305567e59b0ea57740c4d5
Nastya Boyuka <bo25nastya715@gmail.com>
2025-12-17 21:11:09 +0200

    Fix formatting and avoid duplicated error formatting

--- app/main.py
+++ app/main.py
@@ -1,18 +1,23 @@
 def format_linter_error(error: dict) -> dict:
-    return {"line": error["line_number"],
-            "column": error["column_number"],
-            "message": error["text"],
-            "name": error["code"],
-            "source": "flake8"}
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8",
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [format_linter_error(e) for e in errors],
-            "path": file_path,
-            "status": ("passed" if not [format_linter_error(e) for e in errors]
-                       else "failed")}
+    return {
+        "errors": [format_linter_error(e) for e in errors],
+        "path": file_path,
+        "status": "failed" if errors else "passed",
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(path, errs)
-            for path, errs in linter_report.items()]
+    return [
+        format_single_linter_file(path, errors)
+        for path, errors in linter_report.items()
+    ]
